f961c8f520e11415a1b0ab7a0756405f5fb4da54
Bianca Barbosa <yume_shiro@hotmail.com>
2022-01-03 00:04:00 -0300

    feat: creating todolist project

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+
+
+    }
+
+}
